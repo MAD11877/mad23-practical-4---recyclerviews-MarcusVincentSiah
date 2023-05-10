8cff2f49a233d895baa738455d1521f20c28fd6b
MarcusSiah <s10220964@connect.np.edu.sg>
2023-05-10 23:03:19 +0800

    pushing week 4 work into week 4 github

--- app/src/main/java/com/example/tempfile/MainActivity2.java
+++ app/src/main/java/com/example/tempfile/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.tempfile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,44 +12,71 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity2 extends AppCompatActivity {
+    ArrayList<User> activityList;
+    User user;
+    int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.button);
-        boolean[] variable = {false};
-        User user = new User("Hello World!", "Lorem ipsum dolor sit amet, consecttur adipiscing elit, sed do eiusmod tempor incidiunt ut labore et dolore magna aliqua\"", false);
-        EditText name = findViewById(R.id.editTextText);
-        TextView description = findViewById(R.id.descriptionTextView);
+//        boolean[] variable = {false};
+//        User user = new User("Hello World!", "Lorem ipsum dolor sit amet, consecttur adipiscing elit, sed do eiusmod tempor incidiunt ut labore et dolore magna aliqua\"", false);
+//        EditText name = findViewById(R.id.editTextText);
+//        TextView description = findViewById(R.id.descriptionTextView);
+//        name.setText(user.getName());
+//        description.setText(user.getDescription());
 
-        name.setText(user.getName());
-        description.setText(user.getDescription());
+        Bundle extras = getIntent().getExtras();
+        String name = extras.getString("Name");
+        String description = extras.getString("Description");
+        boolean bool = extras.getBoolean("Bool", false);
+        position = extras.getInt("position");
 
-        // name.setOnKeyListener(new);
+
+
+        user = new User(name, description, bool);
+        if (bool == true){
+            btn.setText("Follow");
+        }
+        else if (bool == false){
+            btn.setText("Unfollow");
+        }
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                user.followed = !user.followed; //Default bool will be FALSE
-                btn.setText((user.followed)?"Follow":"Unfollow");
+                if (user.followed == true) {
+                    user.followed = false;
+                    btn.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
 
-                if (user.followed == false){
+                } else {
+                    user.followed = true;
+                    btn.setText("Follow");
                     Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
                 }
-                else if (user.followed == true){
-                    Toast.makeText(getApplicationContext(), "Unfollow", Toast.LENGTH_SHORT).show();
-                }
-//                if (variable[0] == false){
-//                    btn.setText("Unfollow");
-//                    variable[0] = true;
-//                }
-//                else if (variable[0] == true){
-//                    btn.setText("Follow");
-//                    variable[0] = false;
-//                }
 
+                if(activityList == null){
+                    loadData();
+                }
+
+                activityList.get(position).followed = user.followed;
+                saveData();
+//                Bundle extras = new Bundle();
+//                extras.putBoolean("hasFollowed", user.followed);
+//                extras.putInt("position", position);
+//                Intent returnIntent = new Intent();
+//                returnIntent.putExtras(extras);
+//                setResult(Activity.RESULT_OK, returnIntent);
+//                finish();
             }
         });
 
@@ -67,6 +96,24 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    private void loadData(){
+        SharedPreferences sp = getSharedPreferences("contactDb", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sp.getString("users", null);
+        Type type = new TypeToken<ArrayList<User>>() {}.getType();
+        activityList = gson.fromJson(json, type);
+    }
+
+    private void saveData(){
+        SharedPreferences sp = getSharedPreferences("contactDb", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(activityList);
+        editor.putString("users", json);
+        editor.apply();
 
     }
 }
